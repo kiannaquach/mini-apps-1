e98bd6abcb757257f7ab080e78a37f9809d74c04
Kianna <kiannaquach.kq@gmail.com>
2018-05-22 15:41:00 -0700

    working on csv conversion

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,22 @@
-
-
+// console.log('hello');
+$(document).ready(function() {
+		$('button').on('click', function(e) {
+			var textAreaData = $('textarea#data').val();
+			e.preventDefault();
+			// console.log(textAreaData)
+			$.ajax({
+				url: '/csv',
+				method: 'POST',
+				contentType: 'application/json',
+				data: textAreaData,
+				success: function(data) {
+					console.log('this is data ', data)
+				},
+				error: function(err) {
+					console.log('you have an err: ', err)
+				}
+			});
+		});
+		
+	}
+);
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,47 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}));
 
+app.get('/csv', (req, res) => 
+	console.log('this is responseeee: ', res)
+)
+
+function convertToCSV(obj) {
+	var keys = Object.keys(obj);
+	var line = '';
+
+	var firstLine = keys.slice(0, keys.length-1).join(',') + '\n';
+	var parsedObj = JSON.parse(obj);
+
+	
+	// // for (var i = 0; i < obj.length; i++) {
+	// // 	for (key in obj[i]) {
+
+	// // 	}
+	// // }
+	// var result = [];
+	// var eachLine = '';
+
+	// for (var key in obj) {
+	// 	if (key !== 'children') {
+	// 		line = result.push(obj[key]).join(',') + '/n';
+	// 	}
+	// }
+}
+
+app.post('/csv', (req, res) => {
+	// console.log('typeof---', typeof JSON.stringify(req.body));
+	console.log('this is response: ', req.body)
+	// convertToCSV(req.body);
+	// res.sendStatus(201)
+});
+
+app.listen(3000, () => console.log('listening on port 3000'))
+
+module.exports.app = app;
+
+/** STATIC ASSETS **/
+app.use(express.static('client'));
 
